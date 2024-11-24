c0c1e1779922321ea9d65ce5b3b562c8373adb9f
Adam Gent <adam.gent@snaphop.com>
2024-11-23 21:41:57 -0500

    Remove unused import

--- kiwi-kvs/src/test/java/io/jstach/kiwi/kvs/DefaultSedParserPropertiesTest.java
+++ kiwi-kvs/src/test/java/io/jstach/kiwi/kvs/DefaultSedParserPropertiesTest.java
@@ -1,7 +1,6 @@
 package io.jstach.kiwi.kvs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Objects;
 import java.util.stream.Stream;
