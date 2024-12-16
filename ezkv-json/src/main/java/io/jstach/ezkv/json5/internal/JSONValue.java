916c395919652769ec25e5ad8a0455afffff1519
Adam Gent <adam.gent@snaphop.com>
2024-12-16 15:11:08 -0500

    Working implementation

---
+++ ezkv-json/src/main/java/io/jstach/ezkv/json5/internal/JSONValue.java
@@ -0,0 +1,36 @@
+package io.jstach.ezkv.json5.internal;
+
+import org.jspecify.annotations.Nullable;
+
+public sealed interface JSONValue permits JSONObject, JSONArray, JSONValue.JSONString, JSONValue.JSONNumber, JSONValue.JSONBool, JSONValue.JSONNull {
+	
+	@Nullable Object value();
+	
+	record JSONString(String value) implements JSONValue {}
+	record JSONNumber(Number value) implements JSONValue {}
+	
+	enum JSONBool implements JSONValue {
+		FALSE {
+			@Override
+			public Object value() {
+				return false;
+			}
+		},
+		TRUE {
+			@Override
+			public Object value() {
+				return true;
+			}
+		};
+	}
+	
+	enum JSONNull implements JSONValue {
+		NULL;
+		
+		@Override
+		public @Nullable Object value() {
+			return null;
+		}
+	}
+
+}
---
+++ ezkv-json/src/main/java/io/jstach/ezkv/json5/internal/package-info.java
@@ -0,0 +1,2 @@
+@org.jspecify.annotations.NullMarked
+package io.jstach.ezkv.json5.internal;
