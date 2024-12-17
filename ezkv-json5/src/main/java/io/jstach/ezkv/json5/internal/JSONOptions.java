8552e7911d009fad8ea2a5f95931ffdea1232be8
Adam Gent <adam.gent@snaphop.com>
2024-12-17 12:43:35 -0500

    Move flags to enumset

--- ezkv-json5/src/main/java/io/jstach/ezkv/json5/internal/JSONOptions.java
+++ ezkv-json5/src/main/java/io/jstach/ezkv/json5/internal/JSONOptions.java
@@ -23,6 +23,13 @@
  */
 package io.jstach.ezkv.json5.internal;
 
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
+import io.jstach.ezkv.json5.internal.JSONValue.JSONArray;
+import io.jstach.ezkv.json5.internal.JSONValue.JSONObject;
+
 /**
  * Represents options for JSON processing, including stringifying and parsing behaviors.
  *
@@ -30,116 +37,7 @@ package io.jstach.ezkv.json5.internal;
  */
 public record JSONOptions(
 
-		/**
-		 * Whether instants should be stringified as Unix timestamps. If this is
-		 * {@code false}, instants will be stringified as strings (according to
-		 * <a href="https://datatracker.ietf.org/doc/html/rfc3339#section-5.6">RFC 3339,
-		 * Section 5.6</a>).
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONStringify Stringify}-only option</i>
-		 */
-		boolean stringifyUnixInstants,
-
-		/**
-		 * Whether stringifying should only yield ASCII strings. All non-ASCII characters
-		 * will be converted to their Unicode escape sequence (<code>&#92;uXXXX</code>).
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONStringify Stringify}-only option</i>
-		 */
-		boolean stringifyAscii,
-
-		/**
-		 * Whether {@code NaN} should be allowed as a number.
-		 * <p>
-		 * Default: {@code true}
-		 */
-		boolean allowNaN,
-
-		/**
-		 * Whether {@code Infinity} should be allowed as a number. This applies to both
-		 * {@code +Infinity} and {@code -Infinity}.
-		 * <p>
-		 * Default: {@code true}
-		 */
-		boolean allowInfinity,
-
-		/**
-		 * Whether invalid Unicode surrogate pairs should be allowed.
-		 * <p>
-		 * Default: {@code true}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowInvalidSurrogates,
-
-		/**
-		 * Whether strings should be single-quoted ({@code '}) instead of double-quoted
-		 * ({@code "}). This also includes a {@link JSONObject JSONObject's} member names.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONStringify Stringify}-only option</i>
-		 */
-		boolean quoteSingle,
-
-		/**
-		 * Whether binary literals ({@code 0b10101...}) should be allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowBinaryLiterals,
-
-		/**
-		 * Whether octal literals ({@code 0o567...}) should be allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowOctalLiterals,
-
-		/**
-		 * Whether hexadecimal floating-point literals (e.g. {@code 0xA.BCp+12}) should be
-		 * allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowHexFloatingLiterals,
-
-		/**
-		 * Whether Java-style digit separators ({@code 123_456}) should be allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowJavaDigitSeparators,
-
-		/**
-		 * Whether C-style digit separators ({@code 123'456}) should be allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowCDigitSeparators,
-
-		/**
-		 * Whether 32-bit Unicode escape sequences ({@code \U00123456}) should be allowed.
-		 * <p>
-		 * Default: {@code false}
-		 * <p>
-		 * <i>This is a {@link JSONParser Parser}-only option</i>
-		 */
-		boolean allowLongUnicodeEscapes,
+		Set<JSONParserFlag> flags,
 
 		/**
 		 * Specifies the behavior when the same key is encountered multiple times within
@@ -150,7 +48,10 @@ public record JSONOptions(
 		 * <i>This is a {@link JSONParser Parser}-only option</i>
 		 */
 		DuplicateBehavior duplicateBehaviour) {
-
+	
+	public boolean isFlag(JSONParserFlag flag) {
+		return flags.contains(flag);
+	}
 	/**
 	 * An enum containing all supported behaviors for duplicate keys.
 	 *
@@ -177,6 +78,67 @@ public record JSONOptions(
 		DUPLICATE
 
 	}
+	
+	/**
+	 * Represents flags for enabling or disabling specific parsing features in the JSON Parser.
+	 */
+	public enum JSONParserFlag {
+
+//	    /**
+//	     * Whether binary literals ({@code 0b10101...}) should be allowed.
+//	     * <p>
+//	     * Default: {@code false}
+//	     * <p>
+//	     * <i>This is a {@link JSONParser Parser}-only option</i>
+//	     */
+//	    BINARY_LITERALS(JSONParserOption.FLAG_BINARY_LITERALS),
+//
+//	    /**
+//	     * Whether octal literals ({@code 0o567...}) should be allowed.
+//	     * <p>
+//	     * Default: {@code false}
+//	     * <p>
+//	     * <i>This is a {@link JSONParser Parser}-only option</i>
+//	     */
+//	    OCTAL_LITERALS(JSONParserOption.FLAG_OCTAL_LITERALS),
+
+	    /**
+	     * Whether hexadecimal floating-point literals (e.g. {@code 0xA.BCp+12}) should be allowed.
+	     * <p>
+	     * Default: {@code false}
+	     * <p>
+	     * <i>This is a {@link JSONParser Parser}-only option</i>
+	     */
+	    HEX_FLOATING_LITERALS,
+
+//	    /**
+//	     * Whether Java-style digit separators ({@code 123_456}) should be allowed.
+//	     * <p>
+//	     * Default: {@code false}
+//	     * <p>
+//	     * <i>This is a {@link JSONParser Parser}-only option</i>
+//	     */
+//	    JAVA_DIGIT_SEPARATORS(JSONParserOption.FLAG_JAVA_DIGIT_SEPARATORS),
+//
+//	    /**
+//	     * Whether C-style digit separators ({@code 123'456}) should be allowed.
+//	     * <p>
+//	     * Default: {@code false}
+//	     * <p>
+//	     * <i>This is a {@link JSONParser Parser}-only option</i>
+//	     */
+//	    C_DIGIT_SEPARATORS(JSONParserOption.FLAG_C_DIGIT_SEPARATORS),
+
+	    /**
+	     * Whether 32-bit Unicode escape sequences ({@code \U00123456}) should be allowed.
+	     * <p>
+	     * Default: {@code false}
+	     * <p>
+	     * <i>This is a {@link JSONParser Parser}-only option</i>
+	     */
+	    LONG_UNICODE_ESCAPES;
+	}
+
 
 	static final JSONOptions defaultOptions = new Builder().build();
 
@@ -185,151 +147,10 @@ public record JSONOptions(
 	 */
 	public static class Builder {
 
-		boolean stringifyUnixInstants = false;
 
-		boolean stringifyAscii = false;
+		private EnumSet<JSONParserFlag> flags = EnumSet.noneOf(JSONParserFlag.class);
+		private DuplicateBehavior duplicateBehaviour = DuplicateBehavior.UNIQUE;
 
-		boolean allowNaN = true;
-
-		boolean allowInfinity = true;
-
-		boolean allowInvalidSurrogates = true;
-
-		boolean quoteSingle = false;
-
-		boolean allowBinaryLiterals = false;
-
-		boolean allowOctalLiterals = false;
-
-		boolean allowHexFloatingLiterals = false;
-
-		boolean allowJavaDigitSeparators = false;
-
-		boolean allowCDigitSeparators = false;
-
-		boolean allowLongUnicodeEscapes = false;
-
-		DuplicateBehavior duplicateBehaviour = DuplicateBehavior.UNIQUE;
-
-		/**
-		 * Sets whether instants should be stringified as Unix timestamps.
-		 * @param stringifyUnixInstants the value to set
-		 * @return this builder
-		 */
-		public Builder stringifyUnixInstants(boolean stringifyUnixInstants) {
-			this.stringifyUnixInstants = stringifyUnixInstants;
-			return this;
-		}
-
-		/**
-		 * Sets whether stringifying should only yield ASCII strings.
-		 * @param stringifyAscii the value to set
-		 * @return this builder
-		 */
-		public Builder stringifyAscii(boolean stringifyAscii) {
-			this.stringifyAscii = stringifyAscii;
-			return this;
-		}
-
-		/**
-		 * Sets whether {@code NaN} should be allowed as a number.
-		 * @param allowNaN the value to set
-		 * @return this builder
-		 */
-		public Builder allowNaN(boolean allowNaN) {
-			this.allowNaN = allowNaN;
-			return this;
-		}
-
-		/**
-		 * Sets whether {@code Infinity} should be allowed as a number.
-		 * @param allowInfinity the value to set
-		 * @return this builder
-		 */
-		public Builder allowInfinity(boolean allowInfinity) {
-			this.allowInfinity = allowInfinity;
-			return this;
-		}
-
-		/**
-		 * Sets whether invalid Unicode surrogate pairs should be allowed.
-		 * @param allowInvalidSurrogates the value to set
-		 * @return this builder
-		 */
-		public Builder allowInvalidSurrogates(boolean allowInvalidSurrogates) {
-			this.allowInvalidSurrogates = allowInvalidSurrogates;
-			return this;
-		}
-
-		/**
-		 * Sets whether strings should be single-quoted.
-		 * @param quoteSingle the value to set
-		 * @return this builder
-		 */
-		public Builder quoteSingle(boolean quoteSingle) {
-			this.quoteSingle = quoteSingle;
-			return this;
-		}
-
-		/**
-		 * Sets whether binary literals should be allowed.
-		 * @param allowBinaryLiterals the value to set
-		 * @return this builder
-		 */
-		public Builder allowBinaryLiterals(boolean allowBinaryLiterals) {
-			this.allowBinaryLiterals = allowBinaryLiterals;
-			return this;
-		}
-
-		/**
-		 * Sets whether octal literals should be allowed.
-		 * @param allowOctalLiterals the value to set
-		 * @return this builder
-		 */
-		public Builder allowOctalLiterals(boolean allowOctalLiterals) {
-			this.allowOctalLiterals = allowOctalLiterals;
-			return this;
-		}
-
-		/**
-		 * Sets whether hexadecimal floating-point literals should be allowed.
-		 * @param allowHexFloatingLiterals the value to set
-		 * @return this builder
-		 */
-		public Builder allowHexFloatingLiterals(boolean allowHexFloatingLiterals) {
-			this.allowHexFloatingLiterals = allowHexFloatingLiterals;
-			return this;
-		}
-
-		/**
-		 * Sets whether Java-style digit separators should be allowed.
-		 * @param allowJavaDigitSeparators the value to set
-		 * @return this builder
-		 */
-		public Builder allowJavaDigitSeparators(boolean allowJavaDigitSeparators) {
-			this.allowJavaDigitSeparators = allowJavaDigitSeparators;
-			return this;
-		}
-
-		/**
-		 * Sets whether C-style digit separators should be allowed.
-		 * @param allowCDigitSeparators the value to set
-		 * @return this builder
-		 */
-		public Builder allowCDigitSeparators(boolean allowCDigitSeparators) {
-			this.allowCDigitSeparators = allowCDigitSeparators;
-			return this;
-		}
-
-		/**
-		 * Sets whether 32-bit Unicode escape sequences should be allowed.
-		 * @param allowLongUnicodeEscapes the value to set
-		 * @return this builder
-		 */
-		public Builder allowLongUnicodeEscapes(boolean allowLongUnicodeEscapes) {
-			this.allowLongUnicodeEscapes = allowLongUnicodeEscapes;
-			return this;
-		}
 
 		/**
 		 * Sets the behavior when duplicate keys are encountered.
@@ -340,15 +161,20 @@ public record JSONOptions(
 			this.duplicateBehaviour = duplicateBehaviour;
 			return this;
 		}
+		
+		public Builder flag(JSONParserFlag flag) {
+			this.flags.add(flag);
+			return this;
+		}
 
 		/**
 		 * Builds a new {@link JSONOptions} instance using the values set in this builder.
 		 * @return a new {@link JSONOptions} instance
 		 */
 		public JSONOptions build() {
-			return new JSONOptions(stringifyUnixInstants, stringifyAscii, allowNaN, allowInfinity,
-					allowInvalidSurrogates, quoteSingle, allowBinaryLiterals, allowOctalLiterals,
-					allowHexFloatingLiterals, allowJavaDigitSeparators, allowCDigitSeparators, allowLongUnicodeEscapes,
+			var flags = Collections.<JSONParserFlag>unmodifiableSet(EnumSet.copyOf(this.flags));
+			return new JSONOptions(
+					flags,
 					duplicateBehaviour);
 		}
 
